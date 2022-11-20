96d2a2824763e66d647eb6eaad139b7759e8cc72
Logan Aaron <loganaaron16@gmail.com>
2022-11-19 18:13:15 -0600

    Fixed issues with LinkedList methods

--- LinkedList.py
+++ LinkedList.py
@@ -10,14 +10,14 @@ class LinkedList:
         return self.head
 
     def set_head_node(self, node_data):
-        if get_head_node() == None: 
+        if self.get_head_node() == None: 
             new_node = Node(node_data)
             self.head = new_node # Sets the head of the LinkedList to the node passed as an argument
         else:
             raise ValueError("There is already a head node, please use the set_new_head() method")
 
     def get_previous_node(self, node):
-        starting_node = get_head_node(self)
+        starting_node = self.get_head_node()
         prev_node = None
         
         if starting_node == node:
@@ -31,39 +31,65 @@ class LinkedList:
                 starting_node = starting_node.get_next_node()
 
     def add_node(self, node_to_add):
-        starting_node = get_head_node()
+        starting_node = self.get_head_node()
 
         if starting_node is None:
             raise ValueError("The list you are trying to add to is empty, please add a new head Node to start a list")
 
         while starting_node is not None:
-            if starting_node.get_next_node().get_value() == None: # We reached the end of the list
+            if starting_node.get_next_node() == None: # We reached the end of the list
                 new_node = Node(node_to_add)
                 starting_node.set_next_node(new_node)
-                return f'Added {new_node.get_value()} to the list'
+                return f'Added {new_node.get_nodes_value()} to the list'
             starting_node = starting_node.get_next_node()
 
     def set_new_head(self, node_to_set):
-        head_node = get_head_node()
+        head_node = self.get_head_node()
 
         if head_node is not None:
             new_node = Node(node_to_set)
             new_node.set_next_node(head_node) # Set the new Node to point at the previous head Node
         elif head_node is None:
-            set_head_node(Node(node_to_set)) # Sets the head Node to the node to set as head if there is not head Node
+            self.set_head_node(Node(node_to_set)) # Sets the head Node to the node to set as head if there is not head Node
 
     def remove_node(self, node_to_remove): # Removes the Node from the list that contains the value of the argument passed in
-        if get_head_node(self) is not None:
-            starting_node = get_head_node() # Retrieves the Head Node
+        if self.get_head_node() is not None:
+            starting_node = self.get_head_node() # Retrieves the Head Node
             prev_node = None
 
             while starting_node is not None: # Starts at the head Node
-                if starting_node == node_to_remove:
+                if starting_node.get_nodes_value() == node_to_remove:
                     starting_node_next = starting_node.get_next_node() # Getting the next Node to set Previous Node to point to next
                     prev_node.set_next_node(starting_node_next)
-                    return f'Successfully removed {starting_node}'
+                    return f'Successfully removed {starting_node.get_nodes_value()}'
                 prev_node = starting_node
                 starting_node = starting_node.get_next_node()
         else:
             return -1 
 
+    def get_node_index(self, node_to_find):
+        count = 0
+        
+        starting_node = self.get_head_node() # Start at the beginning of the list
+
+        while starting_node is not None:
+            if starting_node.get_nodes_value() == node_to_find:
+                count += 1
+                return count
+            count += 1
+            starting_node = starting_node.get_next_node()
+
+    def stringify_list(self):
+        string_list = ""
+
+        starting_node = self.get_head_node()
+
+        while starting_node is not None:
+            if starting_node.get_next_node() == None:
+                string_list += starting_node.get_nodes_value()
+                starting_node = starting_node.get_next_node()
+            else:
+                string_list += starting_node.get_nodes_value() + ", "
+                starting_node = starting_node.get_next_node()
+
+        return string_list
--- main.py
+++ main.py
@@ -2,10 +2,24 @@
 from Node import Node
 from LinkedList import LinkedList
 
-my_list = LinkedList
+my_list = LinkedList()
 
 my_list.set_head_node("Logan") # Sets the node we created to be the head node of the list
 
-my_list.add_node("Kristen") # Adds a new node to the end of the list
+my_list.add_node("Bob") # Adds a new node to the end of the list
 
-print(my_list)
+my_list.add_node("Joe")
+
+my_list.add_node("Jeff")
+
+head_node = my_list.get_head_node()
+print(head_node.get_nodes_value())
+
+print(my_list.get_node_index("Bob"))
+
+print(my_list.get_head_node())
+
+my_list.remove_node("Joe")
+
+stringed_list = my_list.stringify_list()
+print(stringed_list)
